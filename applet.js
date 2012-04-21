085aba100c2f5dc6cf4b3d80e52204c312e41e59
Elliot Benjamin <elbenj.droid@gmail.com>
2012-04-20 20:49:23 -0400

    Right-click launch terminal for most locations

--- applet.js
+++ applet.js
@@ -2,6 +2,10 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
 const Applet = imports.ui.applet;
+const Clutter = imports.gi.Clutter;
+const Cinnamon = imports.gi.Cinnamon;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 
 const Gettext = imports.gettext;
 const _ = Gettext.gettext;
@@ -17,16 +21,38 @@ function MyPopupMenuItem()
 MyPopupMenuItem.prototype =
 {
 		__proto__: PopupMenu.PopupBaseMenuItem.prototype,
-		_init: function(icon, text, params)
+		_init: function(icon, text, loc, params)
 		{
 			PopupMenu.PopupBaseMenuItem.prototype._init.call(this, params);
-                        let shell_icon = new St.Icon({icon_name: "bash", icon_size: ICON_SIZE, icon_type: St.IconType.FULLCOLOR});
 			this.icon = icon;
+                        this.loc = loc;
 			this.addActor(this.icon);
+                        this.labeltext = text;
 			this.label = new St.Label({ text: text });
 			this.addActor(this.label);
-                        this.addActor(shell_icon);
-		}
+		},
+
+                _onButtonReleaseEvent: function (actor, event)
+                {
+                        if ( Cinnamon.get_event_state(event) & Clutter.ModifierType.BUTTON1_MASK ) {
+                            this.activate(event);
+                        } else if (Cinnamon.get_event_state(event) & Clutter.ModifierType.BUTTON3_MASK) {
+                            if (this.loc) {
+                                if (this.loc == "special:home") {
+                                    this.loc = Gio.file_new_for_path(GLib.get_home_dir()).get_uri().replace('file://','');
+                                } else if (this.loc == "special:desktop") {
+                                    this.loc = Gio.file_new_for_path(GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP)).get_uri().replace('file://','');
+                                } else if (this.loc == "root") {
+                                    this.loc = "/";
+                                } else if (this.loc == "special:connect") {
+                                    return true;
+                                }
+                                Main.Util.spawnCommandLine("gnome-terminal --working-directory="+this.loc);
+                            }
+                        }
+                        this.menu.toggle();
+                        return true;
+                }
 };
 
 function MyMenu(launcher, orientation) {
@@ -84,7 +110,8 @@ MyApplet.prototype = {
 			// Display default places
 			for ( placeid; placeid < this.defaultPlaces.length; placeid++) {
 				let icon = this.defaultPlaces[placeid].iconFactory(ICON_SIZE);
-				this.placeItems[placeid] = new MyPopupMenuItem(icon, _(this.defaultPlaces[placeid].name));
+				this.placeItems[placeid] = new MyPopupMenuItem(icon, _(this.defaultPlaces[placeid].name),
+                                    this.defaultPlaces[placeid].id.replace('bookmark:file://',''));
 				this.placeItems[placeid].place = this.defaultPlaces[placeid];
 
 				this.menu.addMenuItem(this.placeItems[placeid]);
@@ -99,15 +126,15 @@ MyApplet.prototype = {
 			
 			this.menu.addMenuItem(this.computerItem);
 			this.computerItem.connect('activate', function(actor, event) {
-                Main.Util.spawnCommandLine("nautilus computer://");
+                            Main.Util.spawnCommandLine("nautilus computer://");
 			});
 			
 			let icon = new St.Icon({icon_name: "harddrive", icon_size: ICON_SIZE, icon_type: St.IconType.FULLCOLOR});
-			this.filesystemItem = new MyPopupMenuItem(icon, _("File System"));
+			this.filesystemItem = new MyPopupMenuItem(icon, _("File System"), "root");
 			
 			this.menu.addMenuItem(this.filesystemItem);
 			this.filesystemItem.connect('activate', function(actor, event) {
-                Main.Util.spawnCommandLine("/usr/bin/epmupdater -r root");
+                            Main.Util.spawnCommandLine("/usr/bin/epmupdater -r root");
 			});
 			
 			// Separator
@@ -117,9 +144,9 @@ MyApplet.prototype = {
 			// Display default bookmarks
 			for ( bookmarkid; bookmarkid < this.bookmarks.length; bookmarkid++, placeid++) {
 				let icon = this.bookmarks[bookmarkid].iconFactory(ICON_SIZE);
-				this.placeItems[placeid] = new MyPopupMenuItem(icon, _(this.bookmarks[bookmarkid].name));
+				this.placeItems[placeid] = new MyPopupMenuItem(icon, _(this.bookmarks[bookmarkid].name),
+                                    this.bookmarks[bookmarkid].id.replace('bookmark:file://',''));
 				this.placeItems[placeid].place = this.bookmarks[bookmarkid];
-
 				this.menu.addMenuItem(this.placeItems[placeid]);
 				this.placeItems[placeid].connect('activate', function(actor, event) {
 					actor.place.launch();
